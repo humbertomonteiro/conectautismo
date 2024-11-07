ee4c8b5e88b4e7dd6b28d400b27386b873245e94
Humberto Monteiro <humbertoaraujox@icloud.com>
2024-11-07 12:03:44 -0300

    att link button top

--- src/components/shared/TopPromoBar/index.jsx
+++ src/components/shared/TopPromoBar/index.jsx
@@ -1,6 +1,6 @@
 import styles from "./topPromoBar.module.css";
-import Contagem from "../Contagem";
-import ButtonPretty from "../ButtonPretty";
+// import Contagem from "../Contagem";
+// import ButtonPretty from "../ButtonPretty";
 
 export default function TopPromoBar() {
   return (
@@ -8,7 +8,12 @@ export default function TopPromoBar() {
       {/* <span>Contagem regressiva</span> */}
       {/* <Contagem dateEvent={"07 nov 2024"} /> */}
       <p>DISPONIVEL POR TEMPO LIMITADO</p>
-      <a href="#formEvent">GARANTIR AGORA</a>
+      <a
+        href="https://pay.hotmart.com/A96222789P?off=2bsyn86r&bid=1730990288806"
+        target="_blank"
+      >
+        GARANTIR AGORA
+      </a>
       {/* <ButtonPretty text={"Garanta sua vaga"} link={"#formEvent"} /> */}
     </div>
   );
