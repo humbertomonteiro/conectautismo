6ff39b7fa7f07171f29430c1b6307aeff25a2126
Humberto Monteiro <humbertoaraujox@icloud.com>
2026-02-09 19:31:04 -0300

    ajuste no TopPromoBar

--- src/components/shared/TopPromoBar/index.jsx
+++ src/components/shared/TopPromoBar/index.jsx
@@ -4,7 +4,7 @@ export default function TopPromoBar() {
   return (
     <div className={styles.container}>
       {/* <span>Contagem regressiva para pré-venda</span> */}
-      <span>Faça sua incrição com valor promocional:</span>
+      <span>ÚLTIMAS VAGAS.</span>
 
       <a href="#tickets2">Aproveitar agora</a>
     </div>
